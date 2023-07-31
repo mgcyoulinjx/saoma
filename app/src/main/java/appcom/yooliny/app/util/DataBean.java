cf3217c573adcbe2e5d6acb215a79dda6c2a08cb
12532 <mgcyoulin@gmail.com>
2023-08-01 00:08:43 +0800

    第一版

---
+++ app/src/main/java/appcom/yooliny/app/util/DataBean.java
@@ -0,0 +1,69 @@
+package appcom.yooliny.app.util;
+
+public class DataBean {
+    private int date;
+    private int id;
+    private String sifudianji_jingei;
+    private String sifudianji_mojia;
+    private String dianji_gongjian;
+    private String dianjji_jinganggunlun;
+    private String dianji_xiuzhengqi;
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSifudianji_jingei() {
+        return sifudianji_jingei;
+    }
+
+    public void setSifudianji_jingei(String sifudianji_jingei) {
+        this.sifudianji_jingei = sifudianji_jingei;
+    }
+
+    public String getSifudianji_mojia() {
+        return sifudianji_mojia;
+    }
+
+    public void setSifudianji_mojia(String sifudianji_mojia) {
+        this.sifudianji_mojia = sifudianji_mojia;
+    }
+
+    public String getDianji_gongjian() {
+        return dianji_gongjian;
+    }
+
+    public void setDianji_gongjian(String dianji_gongjian) {
+        this.dianji_gongjian = dianji_gongjian;
+    }
+
+    public String getDianjji_jinganggunlun() {
+        return dianjji_jinganggunlun;
+    }
+
+    public void setDianjji_jinganggunlun(String dianjji_jinganggunlun) {
+        this.dianjji_jinganggunlun = dianjji_jinganggunlun;
+    }
+
+    public String getDianji_xiuzhengqi() {
+        return dianji_xiuzhengqi;
+    }
+
+    public void setDianji_xiuzhengqi(String dianji_xiuzhengqi) {
+        this.dianji_xiuzhengqi = dianji_xiuzhengqi;
+    }
+
+
+}
